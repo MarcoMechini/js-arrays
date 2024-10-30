700059a58127f00a217f87ea7ed044e9f84e573a
Marco Mechini <mechinimarco35@gmail.com>
2024-10-30 16:36:05 +0100

    feat: alternative first bonus and second one

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,9 +15,24 @@ for (let i = teachers.length - 1; i >= 0; i--) {
   const curTeacher = teachers[i];
   reversedTeachers.push(curTeacher);
 }
+console.log('Con ciclo for', reversedTeachers);
+
+//OPPURE
+const reversedTeachers2 = teachers.toReversed();
+console.log('Con ciclo for', reversedTeachers2);
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const element = teachers[i];
+  if (element.length >= 5) {
+    longNames.push(element);
+  }
+}
+
+console.log('array con nomi superiori ai 5 caratteri', longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
